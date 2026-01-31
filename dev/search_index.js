f7e70a4044009a221a9347f6affa3763ecc240dd
Documenter.jl <documenter@juliadocs.github.io>
2026-01-31 18:38:58 +0000

    build based on 61c773c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PreciseTimestamps","page":"Home","title":"PreciseTimestamps","text":"Documentation for PreciseTimestamps.\n\n","category":"section"},{"location":"#PreciseTimestamps.datevec_to_precisedatetime-Union{Tuple{Array{T}}, Tuple{T}} where T<:Real","page":"Home","title":"PreciseTimestamps.datevec_to_precisedatetime","text":"datevec_to_precisedatetime(datevec::Array{T}) where {T<:Real}\n\nConvert matlab datevec into PreciseDateTime\n\n\n\n\n\n","category":"method"}]
+}
